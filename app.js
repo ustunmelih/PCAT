dcff5979122b23c35e2b59130dc424d3e07ac53a
ustunmelih <ustun.melih.94@gmail.com>
2022-03-22 19:22:26 +0300

    done

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
 const express = require('express')
-const path = require('path')
+const mongoose = require('mongoose')
+const fileUpload = require('express-fileupload')
+const methodOverride = require('method-override')
+const ejs = require('ejs')
+const photoController = require('./controllers/photoControllers')
+const pageController = require('./controllers/pageController')
 
 const app = express()
 
+//connect DB
+mongoose
+  .connect(
+    'mongodb+srv://ustunmelih:65679497@cluster0.r32jb.mongodb.net/test?authSource=admin&replicaSet=atlas-hhp1o1-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log('DB CONNECTED!')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs')
+
+//MIDDLEWARES
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(fileUpload())
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+)
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-})
+//ROUTES
+app.get('/', photoController.getAllPhotos)
+app.get('/photos/:id', photoController.getPhoto)
+app.post('/photos', photoController.createPhoto)
+app.put('/photos/:id', photoController.updatePhoto)
+app.delete('/photos/:id', photoController.deletePhoto)
 
-const port = 8080
+app.get('/about', pageController.getAboutPage)
+app.get('/add', pageController.getAddPage)
+app.get('/photos/edit/:id', pageController.getEditPage)
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`server running on ${port}`)
+  console.log(`Server running on port: ${port}`)
 })
